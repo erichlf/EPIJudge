b3614cb2abc018959a7594f5c69c701c031edde4
Erich L Foster <erichlf@gmail.com>
2018-10-18 22:10:40 -0400

    First Stab at Checking if Tree is a BST

--- epi_judge_cpp/is_tree_a_bst.cc
+++ epi_judge_cpp/is_tree_a_bst.cc
@@ -1,11 +1,20 @@
 #include <memory>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+#include <limits>
+
 using std::unique_ptr;
 
 bool IsBinaryTreeBST(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+  return isBST(tree, std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
+}
+
+bool isBST(const unique_ptr<BinaryTreeNode<int>>& tree, min, max) {
+  if (tree == nullptr) return true;
+  else if (tree->data < min || tree->data > max) return false;
+
+  return isBST(tree->left, min, tree->data) &&
+         isBST(tree->right, tree->data, max);
 }
 
 int main(int argc, char* argv[]) {
